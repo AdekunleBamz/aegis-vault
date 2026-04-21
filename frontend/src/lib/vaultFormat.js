c26ffd5cf6159e21c1af0d7a3e2751b7404f2d3f
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:47:15 +0100

    feat: add formatRewardAmount helper

--- frontend/src/lib/vaultFormat.js
+++ frontend/src/lib/vaultFormat.js
@@ -6,3 +6,5 @@ export const formatLockPeriod = (days) => days + " days";
 export const formatAPYDisplay = (bps) => (bps / 100).toFixed(2) + "%";
 
 export const formatPositionValue = (v) => (v / 1e6).toFixed(6) + " STX";
+
+export const formatRewardAmount = (v) => (v / 1e6).toFixed(6) + " AGS";
