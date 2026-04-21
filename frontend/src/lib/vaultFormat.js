0ef7d40de50490995a3658da3abdcada7cabafac
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:47:14 +0100

    feat: add formatAPYDisplay helper

--- frontend/src/lib/vaultFormat.js
+++ frontend/src/lib/vaultFormat.js
@@ -2,3 +2,5 @@
 export const formatDepositAmount = (v) => (v / 1e6).toFixed(2) + " STX";
 
 export const formatLockPeriod = (days) => days + " days";
+
+export const formatAPYDisplay = (bps) => (bps / 100).toFixed(2) + "%";
