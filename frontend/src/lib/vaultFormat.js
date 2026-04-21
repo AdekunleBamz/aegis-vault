322e6e6cf59720db09dac91bd6ca5f12efa80c28
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:47:14 +0100

    feat: add formatPositionValue helper

--- frontend/src/lib/vaultFormat.js
+++ frontend/src/lib/vaultFormat.js
@@ -4,3 +4,5 @@ export const formatDepositAmount = (v) => (v / 1e6).toFixed(2) + " STX";
 export const formatLockPeriod = (days) => days + " days";
 
 export const formatAPYDisplay = (bps) => (bps / 100).toFixed(2) + "%";
+
+export const formatPositionValue = (v) => (v / 1e6).toFixed(6) + " STX";
